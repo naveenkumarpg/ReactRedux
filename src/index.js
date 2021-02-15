1becdceeb867314453f403cc08612569eb2055d5
Naveen Setty <naveenkumarpg@gmail.com>
2021-02-15 12:28:14 +0000

    adding css.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Hooks/useState";
+import "./css/style.css";
+// import App from "./Hooks/useState/example1";
+// import App from "./Hooks/useState/example2";
+
+// import App from "./Hooks/useEffect/example1";
+// import App from "./Hooks/useEffect/example2";
+import App from "./Hooks/useEffect/example3";
+
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
